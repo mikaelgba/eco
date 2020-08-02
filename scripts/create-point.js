2479219498111c3e28cbe2d6a09a0e6cbc10b41f
mikaelgba <mikaelgba@gmail.com>
2020-08-01 23:11:39 -0300

    up in create-point.js

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -32,13 +32,56 @@ function getCity(event){
     const ufValue = event.target.value
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    city_select.innerHTML = "<option value>selecione uma cidade</option>"
+    city_select.disabled = true
+
+
     fetch(url)
+        
         .then((resp) => {return resp.json()})
         .then(cities_uf => { 
 
             for( const city of cities_uf){
-                city_select.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                city_select.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
             city_select.disabled = false
         }) 
+}
+
+const select_to_collect = document.querySelectorAll(" .itens-grid li")
+
+for(const select of select_to_collect) {
+    select.addEventListener("click", handlerSelectedItens)
+}
+
+const collect_selected = document.querySelector("input[name=itens]")
+let selected_itens = []
+
+function handlerSelectedItens(event){
+
+    const item_li = event.target
+    item_li.classList.toggle("selected")
+    const item_id = item_li.dataset.id
+    /*console.log(item_id)*/
+
+    const alReady_selected = selected_itens.findIndex( item => {
+
+        const item_found = item == item_id
+        return item_found
+    })
+    if( alReady_selected >= 0 ){
+
+        const filter_itens = selected_itens.filter( item => {
+            
+            const item_is_different = item != item_id
+            return item_is_different
+        })
+        selected_itens = filter_itens
+
+    } else { 
+        selected_itens.push(item_id)
+    }
+    
+    console.log(selected_itens)
+    collect_selected.value = selected_itens
 }
